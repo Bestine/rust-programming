4effc98bc749c2c88b2a35100d1e878d79c0123b
Bestine <bestineokinda@gmail.com>
2025-06-06 20:56:40 +0300

    Mutable variables

---
+++ variables/target/debug/deps/variables-04d50ce308428d9f.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/variables/target/debug/deps/variables-04d50ce308428d9f: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/variables/target/debug/deps/variables-04d50ce308428d9f.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/variables/target/debug/variables: /home/the-ape/CurrentProjects/Courses/rust-programming/variables/src/main.rs
