06f43ba34d0124cac16618abfb9123e89eb80373
Bestine <bestineokinda@gmail.com>
2025-06-10 21:28:14 +0300

    Started data-types

---
+++ data-types/target/debug/data-types.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/data-types/target/debug/data-types: /home/the-ape/CurrentProjects/Courses/rust-programming/data-types/src/main.rs
---
+++ data-types/target/debug/deps/data_types-97db70b916886bdc.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/data-types/target/debug/deps/data_types-97db70b916886bdc: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/data-types/target/debug/deps/data_types-97db70b916886bdc.d: src/main.rs
+
+src/main.rs:
