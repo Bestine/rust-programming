c50d0057580dd7b7bf4287db6db189c5f04db282
Bestine <bestineokinda@gmail.com>
2025-06-11 15:06:18 +0300

    Variables Done!

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,18 +2,33 @@ fn main() {
     // Mutability
     let mut x = 5;
     println!("The value of x is: {x}");
-    // x = 6;
-    // println!("The value of x is: {x}");
+
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // Constants
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds: {THREE_HOURS_IN_SECONDS}");
 
     // Shadowing
-    let x = 5;
+    let y = 10; 
 
-    let x = x + 1;
+    let y = y + 4;
 
     {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
+        let y = y * 3; 
+        println!("The value of y in the inner scope is: {y}");
     }
 
-    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+
+    // Use shadowing to create a new variable with the same name 
+    // but a different data type
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("Spaces: {spaces}");
+    
+    // // However if you use 'mut' a compile-time error arises
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
 }
