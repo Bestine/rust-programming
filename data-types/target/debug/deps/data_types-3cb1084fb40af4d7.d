39975cb80a8dfad5758412e4c3736234a7c4ce18
Bestine <bestineokinda@gmail.com>
2025-07-07 17:22:06 +0300

    Recent Builds tests

---
+++ data-types/target/debug/deps/data_types-3cb1084fb40af4d7.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/data-types/target/debug/deps/data_types-3cb1084fb40af4d7: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/data-types/target/debug/deps/data_types-3cb1084fb40af4d7.d: src/main.rs
+
+src/main.rs:
