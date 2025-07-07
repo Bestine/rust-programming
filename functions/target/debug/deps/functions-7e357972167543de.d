ef7c749e58da05d9953415829a701099f7cefa53
Bestine <bestineokinda@gmail.com>
2025-07-07 17:27:14 +0300

    Restarted on functions

---
+++ functions/target/debug/deps/functions-7e357972167543de.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/functions/target/debug/deps/functions-7e357972167543de: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/functions/target/debug/deps/functions-7e357972167543de.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/functions/target/debug/functions: /home/the-ape/CurrentProjects/Courses/rust-programming/functions/src/main.rs
