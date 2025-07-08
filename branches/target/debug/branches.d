d9768c90f0e9116bde3336d766efca3db5ab9164
Bestine <bestineokinda@gmail.com>
2025-07-08 12:18:08 +0300

    Started on Control flow - if, if else, else

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/branches/target/debug/branches: /home/the-ape/CurrentProjects/Courses/rust-programming/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-2eaf85678a521fad.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/branches/target/debug/deps/branches-2eaf85678a521fad: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/branches/target/debug/deps/branches-2eaf85678a521fad.d: src/main.rs
+
+src/main.rs:
