95664afe6758e19c91b0151cff0984d1dd1955ec
Bestine <bestineokinda@gmail.com>
2025-07-09 16:01:52 +0300

    Started on Ownership

---
+++ ownership/target/debug/deps/ownership-aa1c0c76766fb885.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/ownership/target/debug/deps/ownership-aa1c0c76766fb885: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/ownership/target/debug/deps/ownership-aa1c0c76766fb885.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/ownership/target/debug/ownership: /home/the-ape/CurrentProjects/Courses/rust-programming/ownership/src/main.rs
