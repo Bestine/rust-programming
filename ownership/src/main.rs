61059cfa3149a8db078ac72d80da93e1b2328e8e
Bestine <bestineokinda@gmail.com>
2025-07-14 15:34:29 +0300

    Started on reference and borowwing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let s1 = String::from("hello");
+    let s1 = String::from("the table");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{s2}' is {len}.");
+    println!("The length of '{s1}' is {len}.");
 }
 
-fn calculate_length(s: String) -> (String, usize) {
+fn calculate_length(s: &String) -> usize {
     let length = s.len(); // len() returns the length of a String
 
-    (s, length)
+    length
 }
