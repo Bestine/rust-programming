bde56cee45963b00e4ddb0a809bc30e59d2555f6
Bestine <bestineokinda@gmail.com>
2025-07-16 16:00:04 +0300

    Borrowing and references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    let s1 = String::from("the table");
-
-    let len = calculate_length(&s1);
-
-    println!("The length of '{s1}' is {len}.");
+    let reference_to_nothing = no_dangle();
 }
 
-fn calculate_length(s: &String) -> usize {
-    let length = s.len(); // len() returns the length of a String
+fn no_dangle() -> String{
+    let s = String::from("hello");
 
-    length
+    s
 }
