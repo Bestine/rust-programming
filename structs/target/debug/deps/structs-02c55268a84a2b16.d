9ae22f70460336e71b5d34d355e6e34f517538cd
Bestine <bestineokinda@gmail.com>
2025-07-20 20:38:10 +0300

    Started on Structs

---
+++ structs/target/debug/deps/structs-02c55268a84a2b16.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/structs/target/debug/deps/structs-02c55268a84a2b16.d: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/structs/target/debug/deps/structs-02c55268a84a2b16: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/structs/target/debug/structs: /home/the-ape/CurrentProjects/Courses/rust-programming/structs/src/main.rs
