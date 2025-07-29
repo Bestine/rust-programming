1425dcacbc1ed7dc875481f522dbfe27fca3f0a4
Bestine <bestineokinda@gmail.com>
2025-07-29 17:31:48 +0300

    Introduced if...let

---
+++ if__let-control_flow/target/debug/deps/if__let_control_flow-142c1dc21d08a09d.d
@@ -0,0 +1,5 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/if__let-control_flow/target/debug/deps/if__let_control_flow-142c1dc21d08a09d.d: src/main.rs
+
+/home/the-ape/CurrentProjects/Courses/rust-programming/if__let-control_flow/target/debug/deps/if__let_control_flow-142c1dc21d08a09d: src/main.rs
+
+src/main.rs:
---
+++ if__let-control_flow/target/debug/if__let-control_flow.d
@@ -0,0 +1,1 @@
+/home/the-ape/CurrentProjects/Courses/rust-programming/if__let-control_flow/target/debug/if__let-control_flow: /home/the-ape/CurrentProjects/Courses/rust-programming/if__let-control_flow/src/main.rs
